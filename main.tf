4ca65938fd937ed563c4fffb24d68a38c32d0a70
Braunly <braunly.brony@gmail.com>
2018-06-21 13:52:33 +0300

    Rewrite.

--- main.tf
+++ main.tf
@@ -3,76 +3,96 @@ provider "aws" {
 	region = "eu-west-2"
 }
 
-
-
-# Launch configuration for autoscaling group
-resource "aws_launch_configuration" "ecs-launch-configuration" {
-	name = "ecs-launch-configuration"
-	image_id = "ami-3622cf51"  # eu-west-2 ecs optimized ami
-	instance_type = "t2.micro"
-	
-	root_block_device {
-		volume_type = "standard"
-		volume_size = 100
-		delete_on_termination = true
-	}
-	
-	lifecycle {
-		create_before_destroy = true
-	}
-	
-	associate_public_ip_address = "false"
-	key_name = "denysiuk"
-	user_data = "#!/bin/bash\necho ECS_CLUSTER='${aws_ecs_cluster.ecs-cluster.name}' > /etc/ecs/ecs.config"
-}
-
-# Creates autoscaling group
-resource "aws_autoscaling_group" "ecs-autoscaling-group" {
-	max_size = "2"
-	min_size = "1"
-	
-	launch_configuration = "${aws_launch_configuration.ecs-launch-configuration.name}"
-	
-	vpc_zone_identifier = ["subnet-96b8a0ed"]
-	
-	name = "ecs-inc-25"
-}
-
 # Creates ECS cluster
 resource "aws_ecs_cluster" "ecs-cluster" {
 	name = "inc25ecscluster"
 }
 
-
-# Data source with task definition for ECS service
-data "aws_ecs_task_definition" "ecs-task-definition" {
-  task_definition = "${aws_ecs_task_definition.ecs-task-definition.family}"
-}
-
-
-# Creates task definition
-resource "aws_ecs_task_definition" "ecs-task-definition" {
-  family = "inc25"
-
-  container_definitions = <<DEFINITION
-[
+resource "aws_iam_role" "ecs_inc25" {
+	name = "ecs_inc25"
+	assume_role_policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
   {
-    "cpu": 128,
-    "essential": true,
-    "image": "ubuntu",
-    "memory": 512,
-    "memoryReservation": 64,
-    "name": "inc25"
+    "Effect": "Allow",
+    "Principal": {
+      "Service": "ec2.amazonaws.com"
+    },
+    "Action": "sts:AssumeRole"
   }
 ]
-DEFINITION
+}
+EOF
 }
 
-# Creates ECS service with two instances
-resource "aws_ecs_service" "ecs-service" {
-  name          = "inc25ecsservice"
-  cluster       = "${aws_ecs_cluster.ecs-cluster.id}"
-  desired_count = 2
-
-  task_definition = "${aws_ecs_task_definition.ecs-task-definition.family}:${max("${aws_ecs_task_definition.ecs-task-definition.revision}", "${data.aws_ecs_task_definition.ecs-task-definition.revision}")}"
+resource "aws_iam_role_policy" "ecs_inc25" { 
+	name = "ecs_instance_role"
+	role = "${aws_iam_role.ecs_inc25.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecs:CreateCluster",
+        "ecs:DeregisterContainerInstance",
+        "ecs:DiscoverPollEndpoint",
+        "ecs:Poll",
+        "ecs:RegisterContainerInstance",
+        "ecs:StartTelemetrySession",
+        "ecs:Submit*",
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "ecs:StartTask"
+      ],
+      "Resource": "*"
+    }
+  ]
 }
+EOF
+}
+
+resource "aws_iam_instance_profile" "inc25" {
+	name = "inc25_profile"
+	roles = ["${aws_iam_role.ecs_inc25.name}"]
+}
+
+resource "aws_instance" "first" {
+	ami = "ami-3622cf51"  # eu-west-2 ecs optimized ami
+	instance_type = "t2.micro"
+	
+	key_name = "denysiuk"
+	
+	user_data = <<EOF
+#!/bin/bash
+echo ECS_CLUSTER=${aws_ecs_cluster.ecs-cluster.name} >> /etc/ecs/ecs.config
+EOF
+  
+	tags {
+		Name = "INC-25-1"
+	}
+	
+	iam_instance_profile = "${aws_iam_instance_profile.inc25.name}"
+}
+
+resource "aws_instance" "second" {
+	ami = "ami-3622cf51"  # eu-west-2 ecs optimized ami
+	instance_type = "t2.micro"
+	
+	key_name = "denysiuk"
+	
+	user_data = <<EOF
+#!/bin/bash
+echo ECS_CLUSTER=${aws_ecs_cluster.ecs-cluster.name} >> /etc/ecs/ecs.config
+EOF
+	tags {
+		Name = "INC-25-2"
+	}
+	
+	iam_instance_profile = "${aws_iam_instance_profile.inc25.name}"
+}
+
